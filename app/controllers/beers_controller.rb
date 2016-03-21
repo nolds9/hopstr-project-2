8b21f86151d933a6e9e05bc8e0723c2f80dc3f56
Patrick Moran <patrick_moran@mac.com>
2016-03-21 12:44:03 -0400

    beer show page

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -9,9 +9,16 @@ class BeersController < ApplicationController
 
   def create
     @beer = Beer.create!(beer_params)
-    redirect_to beers_path
+    redirect_to beer_path @beer
   end
 
+  def show
+    @beer = Beer.find(params[:id])
+  end
+
+  def edit
+    @beer = Beer.find(params[:id])
+  end
   private
     def beer_params
       params.require(:beer).permit(:name, :brewery, :abv, :img_url, :style)
