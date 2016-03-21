74c60d4931583a67c0662399eba00d76146d9c1c
Patrick Moran <patrick_moran@mac.com>
2016-03-21 12:03:14 -0400

    add user views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160321154246) do
+ActiveRecord::Schema.define(version: 20160321160233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20160321154246) do
     t.string "username"
     t.string "email"
     t.string "password"
+    t.string "img_url"
   end
 
 end
