794636847e76569edffcbbb5b04f2c86e137fc20
Patrick Moran <patrick_moran@mac.com>
2016-03-21 12:08:24 -0400

    added beer view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160321160233) do
+ActiveRecord::Schema.define(version: 20160321160525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160321160233) do
     t.string "img_url"
     t.string "brewery"
     t.string "location"
+    t.string "style"
   end
 
   create_table "users", force: :cascade do |t|
