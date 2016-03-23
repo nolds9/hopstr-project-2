3d7ee15cad4c43231602f03947ffe3a78c626ca7
Patrick Moran <patrick_moran@mac.com>
2016-03-23 18:07:32 -0400

    something

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,6 @@ module BeerTrackerProject2
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
   end
 end
