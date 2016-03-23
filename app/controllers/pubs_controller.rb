42721b1135aeb4a82c9c983a97076e56eef7654a
Patrick Moran <patrick_moran@mac.com>
2016-03-23 14:00:22 -0400

    styling

--- app/controllers/pubs_controller.rb
+++
@@ -1,8 +0,0 @@
-class PubsController < ApplicationController
-
-  def create
-    @beer = Beer.find(params[:id])
-    @pub = Pub.create!(user: @current_user, beer: @beer)
-    redirect_to user_url @current_user
-  end
-end
